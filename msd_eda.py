650fc32a9601b741ddf7bd83eebb1687d9a2acb6
sawalterg <38083974+sawalterg@users.noreply.github.com>
2018-07-29 21:08:14 -0400

    Add files via upload

---
+++ msd_eda.py
@@ -0,0 +1,115 @@
+#! python 3
+# msd_imp.py - recommendation system for million song challenge using implicit
+
+## Import necessary libraries
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from scipy.sparse.linalg import spsolve
+from sklearn.preprocessing import MinMaxScaler
+import sklearn.model_selection as ms
+import implicit
+
+
+
+
+
+# Training data
+train_trip = pd.read_csv('train_triplets.txt', delimiter = '\t', header = None)
+train_trip.columns = ['users', 'songs', 'play count']
+
+# Test data
+test_trip = pd.read_csv('kaggle_visible_evaluation_triplets.txt', delimiter = '\t', header = None, names = ['users', 'songs', 'play count'])
+
+# Combine the two dataframes, as this is an implicit dataset
+
+df = pd.concat([train_trip, test_trip], ignore_index = True)
+
+# Make the userid and songid strings
+df['users'] = df['users'].astype(str)
+df['songs'] = df['songs'].astype(str)
+
+# Inspect data
+
+train_trip.head()
+train_trip.shape
+
+# How many unique users
+
+print(len(df['users'].drop_duplicates()))
+
+# How many unique songs
+
+print(len(df['songs'].drop_duplicates()))
+
+# Check for null values
+
+print(df['play count'].isnull().sum())
+
+# Check the max and min of each column
+
+play_count_max = df['play count'].max()
+play_count_min = df['play count'].min()
+
+
+# Check the most popular songids
+
+song_pop = df.songs.value_counts()
+
+
+# ALS Collaborative Filter using implicit library
+
+# Make user and song into coded factors
+
+df['songs'] = df['songs'].astype("category")
+df['users'] = df['users'].astype("category")
+df['user_id'] = df['users'].cat.codes
+df['song_id'] = df['songs'].cat.codes
+
+
+
+# Create item/user and user/item sparse matrices
+sp_item_user = sp.csr_matrix((df['play count'].astype(float), (df['user_id'], df['song_id'])))
+sp_user_item = sp.csr_matrix((df['play count'].astype(float), (df['song_id'], df['user_id'])))
+
+# Calculate sparsity
+
+matrix_size = sp_item_user.shape[0] * sp_item_user.shape[1]
+non_zero_cells = len(sp_item_user.nonzero()[0])
+
+sparsity = 1 - (non_zero_cells // matrix_size)
+
+
+# Create ALS model
+
+als_fitter = implicit.als.AlternatingLeastSquares(factors = 15, regularization = 0.1, iterations = 10)
+
+# Assign alpha value (linear scaler) Collaborative Filtering for Implicit Feedback Datasets
+# suggests 40 is good starting point
+
+alpha_val = 40
+
+
+# Scale item_user matrix with the alpha value
+
+scaled_item_user = (sp_item_user * alpha_val).astype('double')
+
+
+
+# Fit the scale
+
+als_fitter.fit(scaled_item_user)
+
+
+# Create user recommender - start with one user
+
+
+recommendations = []
+for user in df['user_id']:
+    recommendations.append(als_fitter.recommend(user, sp_user_item))
+
+
+
+
+
+
